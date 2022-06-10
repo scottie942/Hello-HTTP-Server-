8f96e01d08378e9a9ed132dd0d7aee12d100f335
Scottie <scottie@cccareers.org>
2022-06-10 14:30:05 -0700

    http server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code here
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-type": "text"})
+    res.end("Hello World")
+    console.log("Received " + req.method + " request for: " + req.url)
+});
+
+server.listen(8080, () => {
+    console.log("Server is listening on localhost:8080")
+})
